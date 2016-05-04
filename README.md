39797b34bbfae32278d2fbc548df50b9d7457d43
Mateusz Zatorski <mateuszzatorski@gmail.com>
2016-05-04 12:41:31 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+React Native Platform Stylesheet
+===
+
+Seperated out the [F8StyleSheet](https://github.com/fbsamples/f8app/blob/master/js/common/F8StyleSheet.js) to a module.
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Platform } from 'react-native';
+
+export function create(styles) {
+  const platformStyles = {};
+  Object.keys(styles).forEach((name) => {
+    let { ios, android, ...style } = { ...styles[name] };
+    if (ios && Platform.OS === 'ios') {
+      style = { ...style, ...ios };
+    }
+    if (android && Platform.OS === 'android') {
+      style = { ...style, ...android };
+    }
+    platformStyles[name] = style;
+  });
+  return StyleSheet.create(platformStyles);
+}
