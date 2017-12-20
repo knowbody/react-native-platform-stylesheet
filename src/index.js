8b74fba4cace0dbdc467f7965535fbddac76fed6
marcelglaeser <marcelglaeser@users.noreply.github.com>
2017-12-20 09:04:27 +0100

    Update index.js
    add react-native-iphone-x-helper

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
-import { StyleSheet, Platform } from 'react-native';
+import { StyleSheet, Dimensions, Platform } from 'react-native'
+import { isIphoneX } from 'react-native-iphone-x-helper'
 
 export function create(styles) {
   const platformStyles = {};
   Object.keys(styles).forEach((name) => {
-    let { ios, android, ...style } = { ...styles[name] };
+    let { iosx, ios, android, ...style } = { ...styles[name] };
+    if (iosx && isIphoneX()) {
+      style = { ...style, ...iosx };
+    }
     if (ios && Platform.OS === 'ios') {
       style = { ...style, ...ios };
     }
     if (android && Platform.OS === 'android') {
       style = { ...style, ...android };
     }
-
+    
+     if (name === 'iosx' && isIphoneX()) {
+      Object.keys(style).forEach((styleName) => {
+        if (platformStyles[styleName]) {
+          platformStyles[styleName] = { ...platformStyles[styleName], ...style[styleName] };
+        }
+      });
+    }
+    
     if (name === 'ios' && Platform.OS === 'ios') {
       Object.keys(style).forEach((styleName) => {
         if (platformStyles[styleName]) {
@@ -18,7 +30,7 @@ export function create(styles) {
         }
       });
     }
-
+   
     if (name === 'android' && Platform.OS === 'android') {
       Object.keys(style).forEach((styleName) => {
         if (platformStyles[styleName]) {
@@ -27,7 +39,7 @@ export function create(styles) {
       });
     }
 
-    if (name !== 'ios' && name !== 'android') {
+    if ( name !== 'iosx' && name !== 'ios' && name !== 'android') {
       platformStyles[name] = style;
     }
   });
